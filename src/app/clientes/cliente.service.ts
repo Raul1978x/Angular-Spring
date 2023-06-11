4a4177d80e07e3b1ea33b0c6ebd7633ced61b9df
Raul Gomez <gomez.raul.1978.rg@gmail.com>
2023-06-11 20:57:24 -0300

    implementando funcion delete

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,15 +2,36 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './cliente.json'
 import { Cliente } from './cliente';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() {}
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  HttpHeaders: HttpHeaders | { [header: string]: string | string[]; } | undefined;
 
-  getClientes(): Observable<Cliente[]>{
-    return of (CLIENTES);
+  constructor(private Http: HttpClient) { }
+
+  getClientes(): Observable<Cliente[]> {
+    // return of (CLIENTES);
+    return this.Http.get<Cliente[]>(this.urlEndPoint);
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.Http.post<Cliente>(this.urlEndPoint, cliente, { headers: this.HttpHeaders })
+  }
+  
+  getCliente(id: number): Observable<Cliente> {
+    return this.Http.get<Cliente>(`$(this.urlEndPoint)/${id}`)
+  }
+
+  update(cliente: Cliente): Observable<Cliente> {
+    return this.Http.put<Cliente>(`$(this.urlEndPoint)/${cliente.id}`, cliente, {headers: this.HttpHeaders});
+  }
+
+  delete(id: number): Observable<Cliente> {
+    return this.Http.delete<Cliente>(`${this.urlEndPoint})/${id}`, { headers: this.HttpHeaders });
   }
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -8,18 +9,50 @@ import { ClienteService } from './cliente.service';
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.css']
 })
-export class ClientesComponent implements OnInit{
+export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] | undefined ;
+  clientes: Cliente[] | undefined;
 
-  constructor(private ClienteService: ClienteService){}
-  
+  constructor(private ClienteService: ClienteService) { }
+
   ngOnInit(): void {
     this.ClienteService.getClientes().subscribe(
       clientes => this.clientes = clientes
     );
   }
 
-
-
-}
+  delete(cliente: Cliente): void {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    });
+  
+    swalWithBootstrapButtons.fire({
+      title: 'Está Seguro?',
+      text: `Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, elimínalo!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result: any) => {
+      if (result.isConfirmed) {
+        if (typeof cliente.id !== 'undefined') { // Verificar si cliente.id es undefined
+          this.ClienteService.delete(cliente.id).subscribe(
+            response => {
+              this.clientes = this.clientes?.filter(cli => cli !== cliente);
+            }
+          );
+          swalWithBootstrapButtons.fire(
+            'Eliminado!',
+            `Cliente ${cliente.nombre} ${cliente.apellido} ha sido eliminado con éxito!!.`,
+            'success'
+          );
+        }
+      }
+    });
+  }
+}  
